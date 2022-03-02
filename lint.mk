dd5ec469d7760c3b07db4d3c7e15d89a53bfbd82
Gary Rutland <me@garyrutland.com>
2022-03-02 17:22:40 +0000

    Added lint Makefile

---
+++ lint.mk
@@ -0,0 +1,14 @@
+# lint
+
+LINT_VERSION ?= slim-v4
+LINT_DIRECTORY ?= $(PWD)
+
+LINT_CMD = docker run --rm \
+	-e RUN_LOCAL=true \
+	-e IGNORE_GITIGNORED_FILES=true \
+	-e VALIDATE_PHP_PSALM=false \
+	-v $(LINT_DIRECTORY):/tmp/lint \
+	github/super-linter:$(LINT_VERSION)
+
+lint:
+	$(LINT_CMD)
