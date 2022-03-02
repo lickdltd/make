9da9af78011ac03a24e4a6c670acfab0729dd1c7
Gary Rutland <me@garyrutland.com>
2022-03-02 15:00:58 +0000

    Added wildcards

--- terraform.mk
+++ terraform.mk
@@ -11,13 +11,13 @@ TF_DOCKER_CMD = docker run --rm \
 	-v $(TF_DIRECTORY):/terraform \
 	$(TF_DOCKER_ADDITIONAL) hashicorp/terraform:$(TF_VERSION)
 
-tf_init:
+tf_init tf_init_%:
 	$(TF_DOCKER_CMD) init -upgrade
 
-tf_validate:
+tf_validate tf_validate_%:
 	$(TF_DOCKER_CMD) validate
 
-tf_plan:
+tf_plan tf_plan_%:
 ifdef REFRESH
 	$(TF_DOCKER_CMD) plan -compact-warnings -refresh-only
 else ifdef TARGET
@@ -28,7 +28,7 @@ else
 	$(TF_DOCKER_CMD) plan -compact-warnings
 endif
 
-tf_apply:
+tf_apply tf_apply_%:
 ifdef REFRESH
 	$(TF_DOCKER_CMD) apply -compact-warnings -refresh-only
 else ifdef TARGET
@@ -39,35 +39,35 @@ else
 	$(TF_DOCKER_CMD) apply -compact-warnings
 endif
 
-tf_destroy:
+tf_destroy tf_destroy_%:
 	$(TF_DOCKER_CMD) destroy
 
-tf_fmt:
+tf_fmt tf_fmt_%:
 	$(TF_DOCKER_CMD) fmt -recursive
 
-tf_import:
+tf_import tf_import_%:
 	$(TF_DOCKER_CMD) import '$(ADDRESS)' '$(ID)'
 
-tf_state_list:
+tf_state_list tf_state_list_%:
 	$(TF_DOCKER_CMD) state list
 
-tf_state_mv:
+tf_state_mv tf_state_mv_%:
 	$(TF_DOCKER_CMD) state mv '$(SOURCE)' '$(DESTINATION)'
 
-tf_state_rm:
+tf_state_rm tf_state_rm_%:
 	$(TF_DOCKER_CMD) state rm '$(ADDRESS)'
 
-tf_workspace_delete:
+tf_workspace_delete tf_workspace_delete_%:
 	$(TF_DOCKER_CMD) workspace delete '$(TF_WORKSPACE)'
 
-tf_workspace_list:
+tf_workspace_list tf_workspace_list_%:
 	$(TF_DOCKER_CMD) workspace list
 
-tf_workspace_new:
+tf_workspace_new tf_workspace_new_%:
 	$(TF_DOCKER_CMD) workspace new '$(TF_WORKSPACE)'
 
-tf_workspace_select:
+tf_workspace_select tf_workspace_select_%:
 	$(TF_DOCKER_CMD) workspace select '$(TF_WORKSPACE)'
 
-tf_workspace_show:
+tf_workspace_show tf_workspace_show_%:
 	$(TF_DOCKER_CMD) workspace show
