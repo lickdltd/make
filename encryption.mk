32c0b9e92946e7be5dad901182123064a4456050
Gary Rutland <me@garyrutland.com>
2022-03-02 16:23:10 +0000

    Added encryption Makefile

---
+++ encryption.mk
@@ -0,0 +1,17 @@
+# encryption
+
+ENC_VERSION ?= latest
+ENC_DIRECTORY_DECRYPTED ?= $(PWD)/decrypted
+ENC_DIRECTORY_ENCRYPTED ?= $(PWD)/encrypted
+
+ENC_DOCKER_CMD = docker run --rm \
+	-v ~/.aws:/root/.aws:ro \
+	-v $(ENC_DIRECTORY_DECRYPTED):/tmp/decrypted \
+	-v $(ENC_DIRECTORY_ENCRYPTED):/tmp/encrypted \
+	ghcr.io/lickdltd/kms:$(ENC_VERSION)
+
+enc_encrypt enc_encrypt_%:
+	$(ENC_DOCKER_CMD) encrypt --key $(ENC_KMS_KEY) --input-dir /tmp/decrypted --output-dir /tmp/encrypted
+	
+enc_decrypt enc_decrypt_%:
+	$(ENC_DOCKER_CMD) decrypt --key $(ENC_KMS_KEY) --input-dir /tmp/encrypted --output-dir /tmp/decrypted
