baa6913b8d903db0596475bd52e0a80abd61fdb4
Gary Rutland <me@garyrutland.com>
2022-03-22 16:18:21 +0000

    Updated Docker commands to allow wildcards

--- docker.mk
+++ docker.mk
@@ -11,20 +11,20 @@ DKR_COMPOSE_CMD_UP = $(DKR_COMPOSE_CMD) up --abort-on-container-exit --remove-or
 DKR_COMPOSE_CMD_DOWN = $(DKR_COMPOSE_CMD) down --remove-orphans --volumes $(DKR_COMPOSE_ADDITIONAL)
 DKR_COMPOSE_CMD_PUSH = $(DKR_COMPOSE_CMD) push $(DKR_COMPOSE_ADDITIONAL)
 
-dkr_pull:
+dkr_pull dkr_pull_%:
 	$(DKR_COMPOSE_CMD_PULL)
 
-dkr_build:
+dkr_build dkr_build_%:
 	$(DKR_COMPOSE_CMD_BUILD)
 
-dkr_run:
+dkr_run dkr_run_%:
 	$(DKR_COMPOSE_CMD_RUN)
 
-dkr_up:
+dkr_up dkr_up_%:
 	$(DKR_COMPOSE_CMD_UP)
 
-dkr_down:
+dkr_down dkr_down_%:
 	$(DKR_COMPOSE_CMD_DOWN)
 
-dkr_push:
+dkr_push dkr_push_%:
 	$(DKR_COMPOSE_CMD_PUSH)
