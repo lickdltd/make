17bdf7c228dd04e0fc6ee1054cdc0423d6e226c6
Gary Rutland <me@garyrutland.com>
2022-03-25 13:49:29 +0000

    Updated Docker run additional

--- php.mk
+++ php.mk
@@ -1,7 +1,7 @@
 # php
 
 php_composer_%: CONTAINER = cli
-php_composer_%: DKR_COMPOSE_ADDITIONAL = --no-deps
+php_composer_%: DKR_COMPOSE_ADDITIONAL_RUN = --no-deps
 
 php_composer_install: COMMAND = composer install $(PACKAGE)
 php_composer_install: dkr_run dkr_down
