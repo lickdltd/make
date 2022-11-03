621727883d1a57325b29fabe1c380c5af7dd09fb
Gary Rutland <me@garyrutland.com>
2022-11-03 11:37:53 +0000

    Added AWS SSO login

--- aws.mk
+++ aws.mk
@@ -3,5 +3,8 @@ AWS_REGION ?= eu-west-1
 
 AWS_ECR_DOMAIN ?= $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com
 
+aws_sso_login:
+	aws sso login
+
 aws_ecr_login:
 	aws ecr get-login-password --region $(AWS_REGION) | docker login --username AWS --password-stdin $(AWS_ECR_DOMAIN)
