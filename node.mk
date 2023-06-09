3f1badef04b6cf111460e6f89cb9b54af7940ff4
Gary Rutland <garyrutland@lickd.co>
2023-06-09 10:56:38 +0100

    Removed Docker down commands where volumes might be used

--- node.mk
+++ node.mk
@@ -7,16 +7,13 @@ node_npm_install: DKR_COMPOSE_PROJECT = $(NAME)-node-npm-install
 node_npm_install: COMMAND = npm install $(PACKAGE)
 node_npm_install:
 	$(DKR_COMPOSE_CMD_RUN) || { $(DKR_COMPOSE_CMD_DOWN); exit 1; }
-	$(DKR_COMPOSE_CMD_DOWN)
 
 node_npm_update: DKR_COMPOSE_PROJECT = $(NAME)-node-npm-update
 node_npm_update: COMMAND = npm update $(PACKAGE)
 node_npm_update:
 	$(DKR_COMPOSE_CMD_RUN) || { $(DKR_COMPOSE_CMD_DOWN); exit 1; }
-	$(DKR_COMPOSE_CMD_DOWN)
 
 node_cmd_%: CONTAINER = node
 node_cmd_%: DKR_COMPOSE_PROJECT = $(NAME)-node-cmd-$*
 node_cmd_%:
 	$(DKR_COMPOSE_CMD_RUN) || { $(DKR_COMPOSE_CMD_DOWN); exit 1; }
-	$(DKR_COMPOSE_CMD_DOWN)
--- python.mk
+++ python.mk
@@ -7,4 +7,3 @@ python_pip_install: DKR_COMPOSE_PROJECT = $(NAME)-python-pip-install
 python_pip_install: COMMAND = pip install -r requirements.txt
 python_pip_install:
 	$(DKR_COMPOSE_CMD_RUN) || { $(DKR_COMPOSE_CMD_DOWN); exit 1; }
-	$(DKR_COMPOSE_CMD_DOWN)
